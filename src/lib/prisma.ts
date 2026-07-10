07468ce433a6c24a3ffa17663d4fede792a29f70
Ritik7307 <vijayshankarprajapati29@gmail.com>
2026-07-10 22:50:46 +0530

    Fix prisma singleton for production

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,6 +5,6 @@ const globalForPrisma = globalThis as unknown as {
 }
 
 // Forcing a fresh instantiation to pick up new schema changes on hot reload
-export const prisma = new PrismaClient()
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
